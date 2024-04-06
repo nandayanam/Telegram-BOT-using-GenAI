a0873e1570661c30091e7e888b3a74f60f5b698a
Jayasuryan <jayasuryan.chandrasekhar@signetjewelers.com>
2024-04-06 20:17:02 +0530

    Updated changes

--- research/echo_bot.py
+++ research/echo_bot.py
@@ -16,10 +16,14 @@ bot = Bot(token = API_TOKEN)
 dp=Dispatcher(bot)
 
 @dp.message_handler(commands=['start','help'])
-async def command_start_handler(message: types.message):
+async def command_start_handler(message: types.Message):
     
     await message.reply("Hello! I'm a bot that can help you to find the best restaurants")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
     
+dp.message_handler
+async def echo(message: types.Message):
+    """This will return an echo of the message."""
+    await message.reply(message.text)
