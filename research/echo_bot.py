6f82c02cfc952ac9be642836899d9f8eeec18ad0
Jayasuryan <jayasuryan.chandrasekhar@signetjewelers.com>
2024-04-06 19:03:28 +0530

    Update changes

--- research/echo_bot.py
+++ research/echo_bot.py
@@ -16,7 +16,7 @@ bot = Bot(token = API_TOKEN)
 dp=Dispatcher(bot)
 
 @dp.message_handler(commands=['start','help'])
-async def command_start_handler(message: types.message)
+async def command_start_handler(message: types.message):
     
     await message.reply("Hello! I'm a bot that can help you to find the best restaurants")
 
